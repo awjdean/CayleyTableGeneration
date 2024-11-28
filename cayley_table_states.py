7abb1e6265fe38550f98ed6f52456af5703e5706
awjdean <awjdean@gmail.com>
2024-11-28 02:58:57 +0000

    Error catching for get_row and get_column.

--- cayley_table_states.py
+++ cayley_table_states.py
@@ -7,6 +7,26 @@ class CayleyTableStates:
     def __init__(self):
         self.data: CayleyTableStatesDataType = {}
 
+    def get_row(self, row_label: ActionType):
+        if row_label not in self.data:
+            raise KeyError(
+                f"Row label '{row_label}' not found in table."
+                f"Available rows: {list(self.data.keys())}"
+            )
+        return self.data[row_label]
+
+    def get_column(self, column_label: ActionType):
+        first_row = next(iter(self.data.values()))
+        if column_label not in first_row:
+            raise KeyError(
+                f"Column label '{column_label}' not found in table. "
+                f"Available columns: {list(first_row.keys())}"
+            )
+
+        return {
+            row_label: self.data[row_label][column_label] for row_label in self.data
+        }
+
     def __str__(self):
         if not self.data:
             return "cayley_table_states: CayleyTableStatesType = {}"
@@ -15,14 +35,4 @@ class CayleyTableStates:
         df = pd.DataFrame.from_dict(self.data, orient="index")
 
         # Return the string representation of the DataFrame
-        return f"cayley_table_states: CayleyTableStatesType =\n{df}"
-
-    def get_row(self, row_label: ActionType):
-        # TODO: check.
-        return self.data[row_label]
-
-    def get_column(self, column_label: ActionType):
-        # TODO: check.
-        return {
-            row_label: self.data[row_label][column_label] for row_label in self.data
-        }
+        return f"cayley_table_states =\n{df}"
