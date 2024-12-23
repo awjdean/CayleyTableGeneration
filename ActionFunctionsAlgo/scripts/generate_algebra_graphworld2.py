4c0eb76be41d33f95a921125a61e82e4a79932e4
awjdean <awjdean@gmail.com>
2024-12-23 23:04:47 +0000

    Refactor directory structure and simplify imports in Todo.md and generate_algebra_graphworld2.py. Updated Todo.md with new tasks for optimization and import simplification.

--- ActionFunctionsAlgo/scripts/generate_algebra_graphworld2.py
+++ ActionFunctionsAlgo/scripts/generate_algebra_graphworld2.py
@@ -2,7 +2,9 @@ import os
 import sys
 
 # Add the project root directory to the Python path
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+sys.path.append(
+    os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+)
 
 from transformation_algebra.transformation_algebra import TransformationAlgebra
 from utils.type_definitions import AlgebraGenerationMethod
