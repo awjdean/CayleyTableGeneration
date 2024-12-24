59bcae652578855f28d7afc449e2fac9b284ea70
awjdean <awjdean@gmail.com>
2024-12-24 13:58:08 +0000

    Fixed states Cayley method generate scripts.

--- CayleyStatesAlgo/scripts/generate_gridworld2d.py
+++ CayleyStatesAlgo/scripts/generate_gridworld2d.py
@@ -6,17 +6,24 @@ sys.path.append(
     os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 )
 
-
 from transformation_algebra.transformation_algebra import TransformationAlgebra
+from utils.type_definitions import AlgebraGenerationMethod
 from worlds.gridworlds2d.gridworld2d import Gridworld2D
 
+# Create and initialize the world
 world = Gridworld2D(
     grid_shape=(2, 2),
 )
 world.generate_min_action_transformation_matrix()
+
+# Create and generate the algebra
 algebra = TransformationAlgebra(name="gridworld2D")
-algebra.generate(world=world, initial_state=(0, 0))
-print(algebra.cayley_table_states)
-print(algebra.equiv_classes)
-algebra._generate_cayley_table_actions()
-print(algebra.cayley_table_actions)
+algebra.generate(
+    world=world,
+    initial_state=(0, 0),
+    method=AlgebraGenerationMethod.STATES_CAYLEY,
+)
+
+# Check and display properties
+algebra.check_properties()
+algebra.print_properties(False)
