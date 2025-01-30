1b3e9c91e3322b016cd061b658f29cca70344ad5
awjdean <awjdean@gmail.com>
2025-01-30 04:26:49 +0000

    Update generate_algebra_graphworld3.py script for GraphWorld3
    - Modified algebra names to use "graphworld3" instead of "graphworld2"
    - Commented out global algebra save method
    - Adjusted initial state for local algebra generation

--- ActionFunctionsAlgo/scripts/generate_algebra_graphworld4.py
+++ ActionFunctionsAlgo/scripts/generate_algebra_graphworld4.py
@@ -10,14 +10,14 @@ from transformation_algebra.transformation_algebra import TransformationAlgebra
 from transformation_algebra.utils.algebra_generation_methods import (
     AlgebraGenerationMethod,
 )
-from worlds.graphworlds.graphworld3 import GraphWorld3
+from worlds.graphworlds.graphworld4 import GraphWorld4
 
 # Create GraphWorld2 instance
-world = GraphWorld3()
+world = GraphWorld4()
 world.generate_min_action_transformation_matrix()
 
 # Create and generate global transformation algebra
-global_algebra = TransformationAlgebra("graphworld3_global")
+global_algebra = TransformationAlgebra("graphworld4_global")
 global_algebra.generate(world, method=AlgebraGenerationMethod.ACTION_FUNCTION)
 global_algebra.check_properties()
 global_algebra.print_properties()
@@ -26,7 +26,7 @@ global_algebra.print_properties()
 print("\n" + "=" * 80 + "\n")
 
 # Create and generate local transformation algebra starting from state (1,)
-local_algebra = TransformationAlgebra("graphworld3_local")
+local_algebra = TransformationAlgebra("graphworld4_local")
 local_algebra.generate(
     world, initial_state=(0,), method=AlgebraGenerationMethod.LOCAL_ACTION_FUNCTION
 )
